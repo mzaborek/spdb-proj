458d30d8412d15ac80e90e3ef859dd236c1f014f
Unknown <maciekzaborek@o2.pl>
2018-01-17 19:32:19 +0100

    relief alg

--- spdb_spatial.py
+++ spdb_spatial.py
@@ -7,7 +7,6 @@ from sklearn import tree
 import pickle
 
 conn = psycopg2.connect(database = "postgis_24_sample", user = 'postgres', password='dd', host='localhost', port = 5432)
-
 cursor = conn.cursor()
 print ("Connected to database!\n")
 
@@ -25,23 +24,85 @@ cursor.execute("SELECT DISTINCT type from nyc_streets GROUP BY type;")
 streets_types = cursor.fetchall()
 print("streets types: ", len(streets_types))
 data2=[]
-
+ids = []
 for row in census_blocks:
-    cursor.execute("SELECT count(subway.geom) FROM nyc_subway_stations AS subway RIGHT JOIN nyc_census_tracts AS census ON ST_Contains(census.geom, subway.geom) AND  census.tractid = (%s);", (row[0],)) 
+    cursor.execute("SELECT ST_ASText(ST_Envelope(geom)) FROM nyc_census_tracts WHERE tractid=(%s)",  (row[0],))
+    mbb = cursor.fetchone()[0]
+    #cursor.execute("SELECT count(subway.geom) FROM nyc_census_tracts AS census JOIN nyc_subway_stations AS subway  ON ST_Contains(census.geom, subway.geom) AND  census.tractid = (%s);", (row[0],)) 
+    cursor.execute("SELECT count(*)>0 FROM nyc_subway_stations WHERE ST_Contains(ST_GeomFromText((%s)), geom);", (mbb,)) 
     selectRes = cursor.fetchone()
     data2.append([selectRes[0]])
 
     for streets_type in streets_types:
-        cursor.execute("SELECT count(streets.type) FROM nyc_streets AS streets RIGHT JOIN nyc_census_tracts AS census ON ST_Intersects(census.geom, streets.geom) AND streets.type = (%s) AND census.tractid = (%s);", (streets_type, row[0],))
+        #cursor.execute("SELECT count(streets.type) FROM nyc_streets AS streets RIGHT JOIN nyc_census_tracts AS census ON census.tractid = (%s) AND streets.type = (%s) AND ST_Intersects(census.geom, streets.geom);", (row[0],streets_type,))
+        cursor.execute("SELECT count(*)>0 FROM nyc_streets AS streets WHERE streets.type = (%s) AND ST_Intersects(ST_GeomFromText((%s)), streets.geom);", (streets_type,mbb,))
         data2[-1].append(cursor.fetchone()[0])
     
     data2[-1].append(row[2])
-    if(len(data2)%100 == 0):
-        print("Building :", len(data2))
+    ids.append(row[0])
+    if(len(data2)%300 == 0):
+        print("Building :", 100*len(data2)/len(census_blocks), "%")
+
+
+#w data2: stacje metra, ulice, predykat
+
+##RELIEF algorithm
+weights = [0 for i in range(len(data2[0])-1)]
+for i in range (len(ids)-1):
+    cursor.execute("SELECT tractid, ST_ASText((geom)) FROM nyc_census_tracts WHERE tractid=(%s)", (ids[i], ))
+    data = cursor.fetchone()
+    tractid = data[0]
+    geom = data[1]
+    cursor.execute("SELECT tractid FROM nyc_census_tracts WHERE tractid != (%s) ORDER BY geom <-> (%s)", (tractid, geom, ))
+    closestGeoms = cursor.fetchall()
+
+    nearestHit = False
+    nearestMiss = False
+    #znajdź indeks w danych najbliższego
+    #while (not nearestHit and not nearestMiss):
+    for row in closestGeoms:
+        closestIdx = ids.index(row[0])
+        #closestIdx = ids.index(cursor.fetchone()[0])
+        #znaleziono nearestHit
+        if(data2[closestIdx][-1] == data2[i][-1]):
+            nearestHit = closestIdx
+        #znaleziono nearestMiss
+        else:
+            nearestMiss = closestIdx
+        if(nearestHit and nearestMiss):
+            break
+    
+    #uaktualnij wagi dla każdej cechy
+    for weight in range(len(weights)-1):
+        if(data2[i][weight] == data2[nearestHit][weight]):
+            weights[weight] += 1
+        else:
+            weights[weight] -= 1
+        if(data2[i][weight] == data2[nearestMiss][weight]):
+            weights[weight] -= 1
+        else:
+            weights[weight] += 1
+
+    if(i%100 == 0):
+        print("Relief :", i/len(ids)-1)
+
+print(weights)
+
+
+
+
+
+
+
+
+
+
+
+
 
 blocks_columns = len(data2[0])
 
-train, test = train_test_split(data2, test_size=0.05)
+train, test = train_test_split(data2, test_size=0.1)
 print("sizes:", len(train), len(test))
 
 selectors = [row[:blocks_columns-1] for row in train]
